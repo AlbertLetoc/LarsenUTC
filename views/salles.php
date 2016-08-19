8becedfa33734088c09957528dea8d7498360ce7
Albert Letoc <adressepoubelle000@hotmail.fr>
2016-08-19 17:07:30 -0400

    gestion des salles

---
+++ views/salles.php
@@ -0,0 +1,4 @@
+<h2> Salles de répétitions (<a href="index.php?section=salles&act=bas">Salle du bas</a> + <a href="index.php?section=salles&act=studio">Studio Décibels </a>) </h2>
+<center>
+	<iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;height=600&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;src=1f1vi7amvdvo15cpu4m6u7qh38%40group.calendar.google.com&amp;color=%23A32929&amp;src=g7qh169e2c3s6eehbcfsuo3upg%40group.calendar.google.com&amp;color=%23125A12&amp;ctz=Europe%2FParis" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+</center>
---
+++ views/studio.php
@@ -0,0 +1,3 @@
+<center>
+    <iframe src="https://calendar.google.com/calendar/embed?showPrint=0&amp;showCalendars=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=g7qh169e2c3s6eehbcfsuo3upg%40group.calendar.google.com&amp;color=%23125A12&amp;ctz=Europe%2FParis" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+</center>
