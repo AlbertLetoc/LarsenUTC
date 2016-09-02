5065104afa902c1d12fc082ae40619ff50837327
Thewolfs <Thewolfs@users.github.com>
2016-09-02 16:12:39 +0200

    Fix date + auteur news

--- views/blog.php
+++ views/blog.php
@@ -14,7 +14,7 @@
 <div class="col-md-12">
     <div class="row">
     <?php
-        if (is_array($posts['data']) || is_object($posts['data'])) { // evite les bugs en cas de résultat vide de la requete SQL, comme si pas d'articles rédigés dans la periode demandée par ex 
+        if (is_array($posts['data']) || is_object($posts['data'])) { // evite les bugs en cas de résultat vide de la requete SQL, comme si pas d'articles rédigés dans la periode demandée par ex
             if (empty($posts['data'])) {
                 echo "<h3>Aucun article pour la période demandée</h3>";
             }
@@ -25,7 +25,7 @@
                         <div class="row news-title">
                             <div class="col-md-2 news-date">
                                 <div class="row">
-                                 <?php echo '<span class="news-date-day">'.date('d', strtotime($post['post_date'])).'</span><span class="news-date-month">'.utf8_encode(strftime('%b', strtotime($post['post_date']))).'</span><span class="news-date-year">'.date('Y', strtotime($post['post_date'])).'</span>'; ?>
+                                 <?php echo '<div class="col-md-6"><span class="news-date-day">'.date('d', strtotime($post['post_date'])).'</span></div><div class="col-md-6"><span class="news-date-month">'.utf8_encode(strftime('%b', strtotime($post['post_date']))).'</span><span class="news-date-year">'.date('Y', strtotime($post['post_date'])).'</span></div>'; ?>
                                 </div>
                             </div>
                             <div class="col-md-10">
@@ -34,8 +34,17 @@
                         </div>
                         <div class="row news-content">
                             <div class="col-md-2 news-author">
-                                <img class="news-author-img" src="https://demeter.utc.fr/pls/portal30/portal30.get_photo_utilisateur?username=<?php echo $post['post_author']; ?>" alt="Photo auteur"/>
-                                 <?php echo $post['post_author']; ?>
+                                <div class="row">
+                                    <div class="col-md-6 news-author-img">
+                                        <img src="https://demeter.utc.fr/pls/portal30/portal30.get_photo_utilisateur?username=<?php echo $post['post_author']; ?>" alt="Photo auteur"/>
+                                    </div>
+                                    <div class="col-md-6 news-author-name">
+                                        <?php
+                                        //  echo $post['post_author']; ?>
+                                        <span>Clément</span> <span>Losser</span><!-- Temporaire tant que Ginger n'est pas installer -->
+                                        <span>Président</span>
+                                    </div>
+                                </div>
                             </div>
                             <div class="col-md-10">
                                 <?php echo trim_text($post['post_content'], 400); ?>
