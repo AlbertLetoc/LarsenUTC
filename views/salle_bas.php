38eface2bf6e5d042198060b416038cc87e15ad9
Thewolfs <Thewolfs@users.github.com>
2016-09-05 11:30:40 +0200

    Réorganisation code + calendrier responsive

--- views/salle_bas.php
+++ views/salle_bas.php
@@ -1,3 +1,3 @@
-<center>
-	<iframe src="https://calendar.google.com/calendar/embed?showPrint=0&amp;showCalendars=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=1f1vi7amvdvo15cpu4m6u7qh38%40group.calendar.google.com&amp;color=%23A32929&amp;ctz=Europe%2FParis" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
-</center>
+<div class="calendar">
+	<iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=1f1vi7amvdvo15cpu4m6u7qh38%40group.calendar.google.com&amp;color=%23A32929&amp;ctz=Europe%2FParis" frameborder="0" scrolling="no" ></iframe>
+</div>
