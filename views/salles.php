7cded3501ad33b387c66a26bcebe0727c69009bb
Thewolfs <gomen565@gmail.com>
2016-09-06 08:42:03 +0200

    Réorganisation du code et calendrier responsive

--- views/salles.php
+++ views/salles.php
@@ -1,4 +1,20 @@
-<h2> Salles de répétitions (<a href="index.php?section=salles&act=bas">Salle du bas</a> + <a href="index.php?section=salles&act=studio">Studio Décibels </a>) </h2>
-<center>
-	<iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;height=600&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;src=1f1vi7amvdvo15cpu4m6u7qh38%40group.calendar.google.com&amp;color=%23A32929&amp;src=g7qh169e2c3s6eehbcfsuo3upg%40group.calendar.google.com&amp;color=%23125A12&amp;ctz=Europe%2FParis" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
-</center>
+<?php 
+if(isset($_GET['act'])) {
+	if($_GET['act'] == "bas"){
+		include_once('./views/salle_bas.php');
+	}
+	elseif ($_GET['act'] == "studio"){
+		include_once('./views/studio.php');
+	}
+}
+?>
+
+<p>Cette année 2 créneaux de répétition seront des créneaux "libres". Ceci pour anticiper les demandes occasionnelles en cas de besoins particuliers, concert pressant, jours fériés etc... <br/>
+Ces créneaux sont le .... de .... a .... et le .... de .... a .... dans la salle de répétition du bas.<br/>
+Merci d'adresser vos demandes de réservation <b>AU MOINS 1 semaine a l'avance</b> par mail a <a href="mailto:larsen@assos.utc.fr">larsen@assos.utc.fr</a> .<br/>
+Le bureau Larsen, par le biais de son resp' locaux enverra une validation de la réservation <b>AU MOINS 48h avant le créneau.</b><br/>
+<em>Détail des conditions et offres valables a compter du 16/01/2058 jusqu'au 30/02/2013 sur <a href="http://assos.utc.fr/larsen">www.assos.utc.fr/larsen</a>.</em></p>
+
+<p>Pour toute demande d'enregistrement ou de répétition dans le <a href="index.php?section=salles&act=studio">studio Décibels</a> (et ce de maniere exceptionnelle si le créneau libre n'est pas disponible), merci d'adresser un mail a <a href="mailto:decibels.asso.utc@gmail.com"> decibels.asso.utc@gmail.com </a>.</p>
+
+<h3> Charte des locaux </h3>
