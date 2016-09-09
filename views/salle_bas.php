8c22cfb61346ea26a006803fd4ddd17a50b754ea
Thewolfs <gomen565@gmail.com>
2016-09-09 23:04:43 +0200

    Ajout lien calendrier

--- views/salle_bas.php
+++ views/salle_bas.php
@@ -1,3 +1,10 @@
+<div class="row links">
+	<div class="col-md-12">
+		<a href="/larsenUTC/">Accueil</a> > <a href="#">Répéter</a> > <b>Salle du Pic</b>
+	</div>
+</div>
+<div class="splitter">
+</div>
 <div class="calendar">
 	<iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=1f1vi7amvdvo15cpu4m6u7qh38%40group.calendar.google.com&amp;color=%23A32929&amp;ctz=Europe%2FParis" frameborder="0" scrolling="no" ></iframe>
 </div>
