b2b8030b67e61145ba6f6e4f951eb2e127c1d555
Thewolfs <gomen565@gmail.com>
2016-11-24 17:10:35 +0100

    Mise en forme responsive

--- views/salle_bas.php
+++ views/salle_bas.php
@@ -1,15 +1,15 @@
 <div class="row links">
-	<div class="col-md-12">
+	<div class="col-xs-12">
 		<a href="/larsenUTC/">Accueil</a> > <a href="#">Répéter</a> > <b>Salle du Pic</b>
 	</div>
 </div>
 <div class="splitter">
 </div>
 <div class="row title">
-        <div class="col-md-12">
+        <div class="col-xs-12">
             <h1>Planning salle du bas</h1>
         </div>
 </div>
-<div class="calendar" style="height:750px;">
+<div class="calendar col-xs-12">
 	<iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=WEEK&amp;height=750&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;src=1f1vi7amvdvo15cpu4m6u7qh38%40group.calendar.google.com&amp;color=%23B1365F&amp;ctz=Europe%2FParis" style="border-width:0" width="800" height="750" frameborder="0" scrolling="no"></iframe>
 </div>
--- views/side_bar/s_salles.php
+++ views/side_bar/s_salles.php
@@ -1,19 +1,21 @@
-<div class="list-module row">
-    <div class="col-md-12">
-        <div class="row list-module-title">
-            <div class="col-md-12">
-                <h3>A retenir</h3>
+<div class="col-xs-12">
+    <div class="list-module">
+        <div class="col-xs-12">
+            <div class="row list-module-title">
+                <div class="col-xs-12">
+                    <h3>A retenir</h3>
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                Merci de prendre soin des locaux qui vous sont mis à disposition !<br/><br/>
-                En cas de problèmes ou possible changement de planning, merci de prendre contact avec notre resp' locaux.<br/>
-                Merci d'adresser vos demandes de réservations ponctuelles <b>au moins 1 semaine a l'avance</b> par mail au resp' locaux.<br/>
-                Le bureau Larsen, par le biais de son resp' locaux enverra une validation de la réservation <b>AU MOINS 48h avant le créneau.</b><br/>
-                Contact resp' locaux : <a href="mailto:larsen@assos.utc.fr">larsen@assos.utc.fr</a>
-                <br /><br />
-                <a href="/larsenUTC/upload/Reglement_interieur_salles.pdf">Charte des locaux</a>
+            <div class="row">
+                <div class="col-xs-12">
+                    Merci de prendre soin des locaux qui vous sont mis à disposition !<br/><br/>
+                    En cas de problèmes ou possible changement de planning, merci de prendre contact avec notre resp' locaux.<br/>
+                    Merci d'adresser vos demandes de réservations ponctuelles <b>au moins 1 semaine a l'avance</b> par mail au resp' locaux.<br/>
+                    Le bureau Larsen, par le biais de son resp' locaux enverra une validation de la réservation <b>AU MOINS 48h avant le créneau.</b><br/>
+                    Contact resp' locaux : <a href="mailto:larsen@assos.utc.fr">larsen@assos.utc.fr</a>
+                    <br /><br />
+                    <a href="/larsenUTC/upload/Reglement_interieur_salles.pdf">Charte des locaux</a>
+                </div>
             </div>
         </div>
     </div>
